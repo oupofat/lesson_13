d30656fa93bc63bcdc32bfdeae0dbde40467715d
kris hays <krisjhays@gmail.com>
2018-05-19 13:13:00 -0400

    solved the pig latin

--- pig_latin_refactor.py
+++ pig_latin_refactor.py
@@ -1,8 +1,5 @@
-phrase = input("Enter a phrase: ")
-
-words = phrase.split(" ")
-result_words = []
-for word in words:
+def pig_collect_consonants(word):
+    
     pigword = ""
     state = "collect-consonants"
     first_consonants = ""
@@ -16,44 +13,27 @@ for word in words:
         elif state == "collect-rest":
             pigword += letter
     pigword += first_consonants
-#possiable new function?
-    if first_consonants == "":
-        pigword += "y"
-    pigword += "ay"
-    result_words.append(pigword)
-result = " ".join(result_words)
+    return(pigword)
+    
+#takes the word from above and adds the "y" and "ay".
+def change_to_pig_latin(word):
+    new_word = pig_collect_consonants(word)
+    if new_word == "":
+        new_word+="y"
+    new_word += "ay"
+    return (new_word)
 
-print(result)
-
-#added a function to the pig latin problem
-def pig_latin(phrase):
+#This function breaks down and rejoins the phrase.    
+def pig_latin_phrase(phrase):
     words = phrase.split(" ")
     result_words = []
     for word in words:
-        pigword = ""
-        state = "collect-consonants"
-        first_consonants = ""
-        for letter in word:
-            if state == "collect-consonants":
-                if not letter in "aeiou":
-                    first_consonants += letter
-                else:
-                    state = "collect-rest"
-                    pigword += letter
-            elif state == "collect-rest":
-                pigword += letter
-        pigword += first_consonants
-        if first_consonants == "":
-            pigword += "y"
-        pigword += "ay"
-        result_words.append(pigword)
+        final_word = change_to_pig_latin(word)
+        result_words.append(final_word)
+    final_phrase = " ".join(result_words)
+    return (final_phrase)
         
-    result = " ".join(result_words)
-    return result
-phrase = input("Enter a phrase")
-
-result = pig_latin(phrase)
-print (result)
+phrase = "this is it"
 
 
-
+print (pig_latin_phrase(phrase))
